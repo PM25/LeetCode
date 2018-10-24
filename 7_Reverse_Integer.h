a6162e357bd468f2696282e64295d1e224f39ff7
Einz <jason199786109@gmail.com>
2018-10-24 21:03:49 +0800

    Add information

--- 7_Reverse_Integer.h
+++ 7_Reverse_Integer.h
@@ -1,3 +1,12 @@
+// [LeetCode] 7. Reverse Integer
+// https://leetcode.com/problems/reverse-integer/
+//
+// code by Jason Huang aka PM
+// github: https://github.com/PM25
+// website: https://pm25.github.io
+// discord: https://discord.gg/G4jEeDC
+// email: pm.binary@gmail.com
+
 #include <climits>
 
 class Solution{
--- 9_Palindrome_Number.h
+++ 9_Palindrome_Number.h
@@ -1,3 +1,12 @@
+// [LeetCode] 9. Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+//
+// code by Jason Huang aka PM
+// github: https://github.com/PM25
+// website: https://pm25.github.io
+// discord: https://discord.gg/G4jEeDC
+// email: pm.binary@gmail.com
+
 class Solution {
 public:
     bool isPalindrome(int x) {
