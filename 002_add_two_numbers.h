ef97b436fcb0f6e2999bf306755cf0914ebab2af
PM <jason199786109@gmail.com>
2019-08-01 01:14:01 -0500

    done

---
+++ 002_add_two_numbers.h
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int l1_val, l2_val, carry(0);
+        ListNode *root(nullptr), *prev_node;
+
+        while(l1 != nullptr || l2 != nullptr) {
+            if(l1 != nullptr) {
+                l1_val = l1->val;
+                l1 = l1->next;
+            } else {
+                l1_val = 0;
+            }
+
+            if(l2 != nullptr) {
+                l2_val = l2->val;
+                l2 = l2->next;
+            } else {
+                l2_val = 0;
+            }
+
+            int val = (l1_val + l2_val + carry) % 10;
+            carry = (l1_val + l2_val + carry) / 10;
+            if(root == nullptr) {
+                root = new ListNode(val);
+                prev_node = root;
+            } else {
+                prev_node->next = new ListNode(val);
+                prev_node = prev_node->next;
+            }
+        }
+        if(carry) prev_node->next = new ListNode(1);
+
+        return root;
+    }
+
+    ListNode* reverse_list(ListNode* l) {
+        ListNode *prev_node = nullptr,
+                *curr_node = l,
+                *next_node;
+
+        while(curr_node != nullptr) {
+            next_node = curr_node->next;
+            curr_node->next = prev_node;
+            prev_node = curr_node;
+            curr_node = next_node;
+        }
+
+        return prev_node;
+    }
+};
